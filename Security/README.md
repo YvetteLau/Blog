51457f583a01f6c63ced048c667f50c0a277e2fa
YvetteLau <542783088@qq.com>
2019-05-06 23:54:30 +0800

    xss

--- Security/README.md
+++ Security/README.md
@@ -1,21 +1,28 @@
-> 环境安装
+> 环境
 
 1. npm install
-
+2. 本人使用的编辑器是 VSCode(自行安装 Code Runner 插件，用于运行 server.js)
 
 > 反射型XSS攻击
 
 1. 进入 XSS 目录，运行 server.js (启动本地服务器)
-2. 在浏览器中访问 localhost:3000/login.html
+2. 在浏览器中访问 `localhost:3000/login.html`
 3. 使用错误的用户名/密码进行登录(例如: 123 / 123)，会跳向：`http://localhost:3000/error?type=<script>alert('恶意内容')</script>`  
 4. 使用正确的用户名: yvette / yvette 登录，会跳向: `http://localhost:3000/welcome?type=<script>alert('恶意内容')</script>` ;但是我们已经进行了转义，不会再被攻击
 
 > DOM 型XSS攻击
 
-1. 浏览器中访问 localhost:3000
-2. 输入评论内容: 2222<script>alert(1)</script>
+1. 浏览器中访问 `localhost:3000/after.html`
+2. 输入评论内容: `2222<script>alert(1)</script>`
    
     当然啦，如果登录状态，还可以拿cookie等信息~
     还可以悄悄引入其它的js文件过来，可怕~
 
 3. 我们可以对输入的内容进行转义，这样就不会被攻击啦~
+
+> 存储型 XSS 攻击
+
+1. 浏览器中访问 localhost:3000/comments.html
+2. 评论需要先登录，如未登录会自动跳去登录页
+3. 输入评论内容: `2222<script>alert(1)</script>`
+4. 恶意脚本未经转换，存储到了后台。任何用户访问此页面，都会执行恶意脚本。
--- Security/XSS/server.js
+++ Security/XSS/server.js
@@ -6,15 +6,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 
 //设置路径
 app.use(express.static(path.join(__dirname, 'src')));
 app.use(express.static(path.join(__dirname, '../')));
 //将参数转换成对象
 app.use(bodyParser.urlencoded({ extended: true }));
+//req.cookie[xxx] 获取cookie
+app.use(cookieParser());
 
 //用户列表
-let userList = [{ username: 'yvette', password: 'yvette' }, { username: 'liuyan', password: 'liuyan' }];
+let userList = [{ username: 'yvette', password: 'yvette' }, { username: 'star', password: 'star' }];
 
 let SESSION_ID = 'connect.sid';
 let session = {};
@@ -25,7 +29,7 @@ app.post('/api/login', (req, res) => {
     if (user) {
         //用户登录后，给一个标识(cookie登录)
         const cardId = Math.random() + Date.now();
-        session[cardId] = {user};
+        session[cardId] = { user };
         res.cookie(SESSION_ID, cardId);
         res.json({ code: 0 });
     } else {
@@ -34,17 +38,41 @@ app.post('/api/login', (req, res) => {
 
 });
 
-
-//1.反射型XSS攻击: http://localhost:3000/welcome?type=<script>alert('恶意内容')</script>
+//1.反射型XSS攻击: http://localhost:3000/error?type=<script>alert('恶意内容')</script>
 //chrome能够检测到Url上的XSS攻击(可在firefox或者是其它浏览器测试)
-app.get('/welcome', function(req, res) {
+app.get('/error', function (req, res) {
+    res.send(`${req.query.type}`); //拿到 url 上的 type 参数，并返回给前端    
+});
+
+app.get('/welcome', function (req, res) {
     //对查询参数进行编码，避免XSS攻击
-    res.send(`${encodeURIComponent(req.query.type)}`); 
+    res.send(`${encodeURIComponent(req.query.type)}`);
     //对type查询参数进行编码，即可解决当前的XSS攻击(可重启服务查看)
     // res.send(`${encodeURIComponent(req.query.type)}`);
 });
 
-app.get('/error', function(req, res) {
-    res.send(`${req.query.type}`); //拿到 url 上的 type 参数，并返回给前端    
+//评论列表
+let comments = [
+    { username: 'yvette', content: '大家好' },
+    { username: 'yvette', content: '我是刘小夕' },
+    { username: 'star', content: '大家好，我是Star' },
+]
+app.get('/getComments', function (req, res) {
+    res.json({ code: 0, comments });
 });
+
+app.post('/addComment', function (req, res) {
+    //cardId (req.cookies[SESSION_ID])要派上用场啦~
+    let info = session[req.cookies[SESSION_ID]];
+    if (info) {
+        //用户已经登录
+        let username = info.user.username;
+        comments.push({ username, content: req.body.comment });
+        res.json({ code: 0, comments });
+    } else {
+        res.json({ code: 1, error: 'user not logged in.' });
+    }
+});
+
+
 app.listen(3000);
